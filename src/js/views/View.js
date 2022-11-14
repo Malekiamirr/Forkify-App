df14d21efe603e3027e34da743f0d55ecefc0783
Amir Maleki <realmalekiamir@gmail.com>
2022-11-14 11:03:06 +0330

    fix the view.js file

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -5,17 +5,18 @@ export default class View {
   _data;
 
   /**
-   * Render to recieve object to the DOM
+   * Render the received object to the DOM
    * @param {Object | Object[]} data The data to be rendered (e.g. recipe)
-   * @param {boolean} [render = true] If false, create markup string instead of rendering to the DOM
-   * @returns {undefined | string} A markup string is return if render = false
+   * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup string is returned if render=false
    * @this {Object} View instance
-   * @author Amir Maleki
+   * @author Jonas Schmedtmann
    * @todo Finish implementation
    */
   render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
-      return this.rederError();
+      return this.renderError();
+
     this._data = data;
     const markup = this._generateMarkup();
 
@@ -32,19 +33,21 @@ export default class View {
     const newDOM = document.createRange().createContextualFragment(newMarkup);
     const newElements = Array.from(newDOM.querySelectorAll('*'));
     const curElements = Array.from(this._parentElement.querySelectorAll('*'));
+
     newElements.forEach((newEl, i) => {
       const curEl = curElements[i];
       // console.log(curEl, newEl.isEqualNode(curEl));
 
-      // Update change TEXT
+      // Updates changed TEXT
       if (
         !newEl.isEqualNode(curEl) &&
         newEl.firstChild?.nodeValue.trim() !== ''
       ) {
+        // console.log('💥', newEl.firstChild.nodeValue.trim());
         curEl.textContent = newEl.textContent;
       }
 
-      // Update change ATTRIBUES
+      // Updates changed ATTRIBUES
       if (!newEl.isEqualNode(curEl))
         Array.from(newEl.attributes).forEach(attr =>
           curEl.setAttribute(attr.name, attr.value)
@@ -58,41 +61,41 @@ export default class View {
 
   renderSpinner() {
     const markup = `
-    <div class="spinner">
-      <svg>
-        <use href="${icons}#icon-loader"></use>
-      </svg>
-    </div>
+      <div class="spinner">
+        <svg>
+          <use href="${icons}#icon-loader"></use>
+        </svg>
+      </div>
     `;
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
 
-  rederError(message = this._errorMessage) {
+  renderError(message = this._errorMessage) {
     const markup = `
-       <div class="error">
-            <div>
-              <svg>
-                <use href="${icons}#icon-alert-triangle"></use>
-              </svg>
-            </div>
-            <p>${message}</p>
+      <div class="error">
+        <div>
+          <svg>
+            <use href="${icons}#icon-alert-triangle"></use>
+          </svg>
         </div>
+        <p>${message}</p>
+      </div>
     `;
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
 
-  rederMessage(message = this._message) {
+  renderMessage(message = this._message) {
     const markup = `
-    <div class="message">
-    <div>
-      <svg>
-        <use href="${icons}#icon-smile"></use>
-      </svg>
-    </div>
-    <p>${message}</p>
-  </div>
+      <div class="message">
+        <div>
+          <svg>
+            <use href="${icons}#icon-smile"></use>
+          </svg>
+        </div>
+        <p>${message}</p>
+      </div>
     `;
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
